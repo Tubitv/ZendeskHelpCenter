d262fbe0be786d17ead9a22f6ed4684d996dc154
StoyanD <s.dimitrov19@gmail.com>
2017-05-01 18:05:58 -0700

    initial commit

---
+++ app/src/main/java/com/tubitv/tools/demo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tubitv.tools.demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.tubitv.demo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
